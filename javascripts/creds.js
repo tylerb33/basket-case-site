e27cb69f5719312898afee890cf91c0d7cd2be9b
Tyler Barnett <tyler.barnett7190@gmail.com>
2018-02-07 20:02:09 -0600

    removed credentials file

--- javascripts/creds.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let accessToken = '406989814.1677ed0.e4c4933ed0994e8d81af9e1811d1811a';
-
-function getAccessToken() {
-	return accessToken;
-}
-
-module.exports = {getAccessToken};
